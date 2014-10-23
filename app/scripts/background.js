7f89fd23423e8d64f43c3017f2e03df88f1321b9
Daniel <daniel@developerdan.com>
2014-10-23 11:35:33 -0400

    fixed length to 56 to match actual X-UIDHs

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -4,7 +4,7 @@ function makeid() {
   var text = '';
   var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-  for( var i=0; i < 5; i++ ) {
+  for( var i=0; i < 56; i++ ) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
   return text;
