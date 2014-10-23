56c73c9d0edd39be76174e3601dad15f7358c9bf
Daniel <daniel@developerdan.com>
2014-10-23 10:44:57 -0400

    Initial setup

---
+++ app/scripts/background.js
@@ -0,0 +1,20 @@
+'use strict';
+
+function makeid() {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for( var i=0; i < 5; i++ ) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
+chrome.webRequest.onBeforeSendHeaders.addListener(
+  function(details) {
+    details.requestHeaders.push({name:'X-UIDH',value: makeid()});
+    return {requestHeaders: details.requestHeaders};
+  },
+  {urls: ['http://*/*']},
+  ['requestHeaders', 'blocking']
+);
